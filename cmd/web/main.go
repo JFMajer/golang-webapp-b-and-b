f7cfdbd34539c1d3876212386ef5d897306c4cd0
JFMajer <jakubmajer@live.com>
2023-01-09 20:20:42 +0100

    created app wide config

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,18 +2,34 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
+	"github.com/JFMajer/golang-webapp-b-and-b/pkg/config"
 	"github.com/JFMajer/golang-webapp-b-and-b/pkg/handlers"
+	"github.com/JFMajer/golang-webapp-b-and-b/pkg/render"
 )
 
 const portNumber = ":8080"
 
 // main is main application function
 func main() {
+	var app config.AppConfig
 
-	http.HandleFunc("/", handlers.Home)
-	http.HandleFunc("/about", handlers.About)
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal(err)
+	}
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+
+	render.NewTemplates(&app)
+
+	http.HandleFunc("/", handlers.Repo.Home)
+	http.HandleFunc("/about", handlers.Repo.About)
 
 	fmt.Printf("Starting application on port %s\n", portNumber)
 	http.ListenAndServe(portNumber, nil)
--- pkg/render/render.go
+++ pkg/render/render.go
@@ -6,22 +6,34 @@ import (
 	"net/http"
 	"path/filepath"
 	"text/template"
+
+	"github.com/JFMajer/golang-webapp-b-and-b/pkg/config"
 )
+
+var app *config.AppConfig
+
+// NewTemplates sets the config for the template package
+func NewTemplates(a *config.AppConfig) {
+	app = a
+}
 
 // RenderTemplate function renders a template using html template
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
-	// create a template cache
-	tc, err := createTemplateCache()
-	if err != nil {
-		log.Fatal(err)
+	var tc map[string]*template.Template
+	if app.UseCache {
+		tc = app.TemplateCache
+	} else {
+		tc, _ = CreateTemplateCache()
 	}
+	// create a template cache
+
 	// get requested template from cache
 	t, ok := tc[tmpl]
 	if !ok {
-		log.Fatal(err)
+		log.Fatal("Could not get template from template cache")
 	}
 	buf := new(bytes.Buffer)
-	err = t.Execute(buf, nil)
+	err := t.Execute(buf, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -32,7 +44,7 @@ func RenderTemplate(w http.ResponseWriter, tmpl string) {
 	}
 }
 
-func createTemplateCache() (map[string]*template.Template, error) {
+func CreateTemplateCache() (map[string]*template.Template, error) {
 	myCache := map[string]*template.Template{}
 	//get all of the files named *.page.gohtml from ./templates
 	pages, err := filepath.Glob("./templates/*page.gohtml")
